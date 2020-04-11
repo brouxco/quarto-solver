7bb593421a70885055edf068fc69685b7238157f
Colas Broux <colas.broux@free.fr>
2020-04-12 00:12:55 +0200

    Fixed algorithm Needs comments and tests

--- quarto.py
+++ quarto.py
@@ -4,44 +4,51 @@ import board
 import copy
 import argparse
 
+# Contains the score, losing_piece and player
 boards_scores = dict()
 
 
-# return score, losing_piece
+# return score, losing_piece and player
 def find_losing_piece(board, opponent):
     if hash(board) in boards_scores:
-        # BUG(brouxco): This may be invalid, the board score depends on the
-        # player. We have to return the board score for the player
         return boards_scores[hash(board)]
     if board.is_move_losing():
-        return 1 if opponent else -1, None
+        return 1 if opponent else -1, None, opponent
     if len(board.available_pieces) == 0:
-        return 0, None
+        return 0, None, opponent
     original_board = copy.deepcopy(board)
     pieces_score = [0 for _ in original_board.available_pieces]
     if not opponent:
         for i, p in enumerate(original_board.available_pieces):
             for move in original_board.allowed_moves():
                 board.place(*move, p)
-                score, _ = find_losing_piece(board, True)
-                pieces_score[i] += score
+                score, _, enemy = find_losing_piece(board, True)
+                if enemy:
+                    pieces_score[i] -= score
+                else:
+                    pieces_score[i] += score
                 board = copy.deepcopy(original_board)
         max_piece_index = pieces_score.index(max(pieces_score))
         boards_scores[hash(original_board)] = (
             pieces_score[max_piece_index],
-            original_board.available_pieces[max_piece_index]
+            original_board.available_pieces[max_piece_index],
+            opponent
         )
     else:
         for i, p in enumerate(original_board.available_pieces):
             for move in original_board.allowed_moves():
                 board.place(*move, p)
-                score, _ = find_losing_piece(board, False)
-                pieces_score[i] += score
+                score, _, enemy = find_losing_piece(board, False)
+                if enemy:
+                    pieces_score[i] += score
+                else:
+                    pieces_score[i] -= score
                 board = copy.deepcopy(original_board)
         min_piece_index = pieces_score.index(min(pieces_score))
         boards_scores[hash(original_board)] = (
             pieces_score[min_piece_index],
-            original_board.available_pieces[min_piece_index]
+            original_board.available_pieces[min_piece_index],
+            opponent
         )
     print(board)
     print(pieces_score)
@@ -62,19 +69,20 @@ def main():
         "--piece",
         help="The piece you were given"
     )
-    parser.add_argument(
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
         "-i",
         "--interactive",
         help="Play your game and interact with the solver",
         action="store_true"
     )
-    parser.add_argument(
+    group.add_argument(
         "-spi",
         "--solve-piece",
         help="Suggest a piece to give to the opponent",
         action="store_true"
     )
-    parser.add_argument(
+    group.add_argument(
         "-spo",
         "--solve-position",
         help="Suggest where to put the piece you were given",
